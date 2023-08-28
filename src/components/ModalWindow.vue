f2df73a9f1aedf972ae6e179b85e2bf968377996
vavasik800 <vasilenok.va@mail.ru>
2023-08-28 12:58:31 +0300

    Add component modal-window

---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="modal fade" :id="id" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-m">
+      <div class="modal-content">
+        <div v-if="header" class="modal-header">
+          <h5  class="modal-title">{{ header }}</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
+        </div>
+        <div class="modal-body">
+          <img v-if="imgBody"
+               :src="imgBody"
+               class="w-75 h-75 mx-auto d-block"
+               alt="img for modal"/>
+          <p class=" mb-0 mt-3">{{ textBody }}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button"
+                  class="btn btn-outline-primary btn-sm"
+                  @click="clickSaveButton"
+          >Сохранить изменения</button>
+          <button type="button" class="btn btn-outline-danger btn-sm" data-bs-dismiss="modal">Отмена</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Modal } from 'bootstrap'
+
+export default {
+  name: "ModalWindow",
+  props: {
+    id: {
+      type: String,
+    },
+    header: {
+      type: String,
+      default: '',
+    },
+    textBody: {
+      type: String,
+    },
+    imgBody: {
+      type: String,
+    }
+  },
+  mounted() {
+    this.theModal = new Modal('#'.concat(this.id))
+  },
+  methods: {
+    clickSaveButton() {
+      this.theModal.hide()
+      this.$emit('clickOk')
+    },
+  }
+}
+</script>
+
+<style scoped>
+.modal-m {
+  max-width: 450px;
+}
+</style>
